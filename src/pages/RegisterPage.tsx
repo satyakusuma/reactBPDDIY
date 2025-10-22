7f926683355f708343227ff5bb7eca2988aea894
satyakusuma <satyategar25@gmail.com>
2025-10-22 20:57:00 +0800

    add Register Page

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,9 +1,98 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate, Link } from "react-router-dom";
 
-const RegisterPage = () => {
+const RegisterPage: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<string>("");
+  const [success, setSuccess] = useState<string>("");
+  const { register } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError("");
+    setSuccess("");
+    
+    const role = "nasabah";
+
+    try {
+      await register(email, password, role);
+      setSuccess(
+        "Registrasi berhasil! Anda akan diarahkan ke halaman login..."
+      );
+      setTimeout(() => {
+        navigate("/login"); // Arahkan ke login setelah sukses
+      }, 2000);
+    } catch (err: any) {
+      setError(err.response?.data?.message || "Registrasi gagal.");
+      console.error(err);
+    }
+  };
+
   return (
-    <div>RegisterPage</div>
-  )
-}
+    <div className="w-full max-w-xs">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      >
+        <h2 className="text-2xl font-bold mb-6 text-center">Register</h2>
+        {error && <p className="text-red-500 text-xs italic mb-4">{error}</p>}
+        {success && (
+          <p className="text-green-500 text-xs italic mb-4">{success}</p>
+        )}
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="email@example.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="******************"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Register
+          </button>
+          <Link
+            to="/login"
+            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+          >
+            Sudah punya akun?
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default RegisterPage
+export default RegisterPage;
