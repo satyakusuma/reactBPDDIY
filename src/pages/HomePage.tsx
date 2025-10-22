60f759c190ae250c8e254ed790a614e8efdbe531
satyakusuma <satyategar25@gmail.com>
2025-10-22 19:51:16 +0800

    add Home Page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="p-4">
+        <h1 className="text-2xl font-bold">Selamat Datang !!</h1>
+        <p>Ini adalah halaman Utama</p>
+    </div>
+  )
+}
+
+export default HomePage
